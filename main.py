a64df0eca93eb90551b8416a99f51ed8d7938e07
ericfloriano <ericfloriano@gmail.com>
2025-04-04 00:33:44 -0300

    Inserir código simples com if

---
+++ main.py
@@ -0,0 +1,12 @@
+# crie um programa que verifique o valor de venda é positivo
+# abra no menu lateral a opção de run and debug
+# no código clique na lateral esquerda (antes do número da linha) para criar os breakpoints que serão utilizados no debugger
+# inicie a depuração do código
+
+quantidade = 20
+valor = -30
+
+if quantidade > 0 and valor > 0:
+    print("valor positivo")
+else:
+    print("valor negativo")
